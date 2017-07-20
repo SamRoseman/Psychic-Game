d624903c162d4be06dba76a18182d92cb156d0bf
Samantha Roseman <samantharoseman@unknown60f81da93de4.att.net>
2017-07-19 17:19:19 -0700

    Wed afternoon commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,77 @@
+
+		var user;
+		var comp;
+		var math;
+		var wins = 0;
+		var losses = 0;
+		var guessesLeft = 10;
+		var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+		var compAnswer = document.getElementById("comp-text");
+		var userGuess = document.getElementById("user-text");
+		var winsText = document.getElementById("wins-text");
+		var lossesText = document.getElementById("losses-text");
+		var guessesText = document.getElementById("guesses-text");
+		var userArray = [];
+		
+
+		// set function that sets math to a random number 0 - 25
+		function getLetter() {
+			math = Math.floor(Math.random() * 25);
+		}
+
+		function startOver() {
+			guessesLeft = 10;
+			getLetter();
+			userArray = [];
+		}
+
+		// calling the function
+		getLetter();
+
+		// listens for user key press
+		document.onkeyup = function(push) {
+		
+			// set variable use to whatever key the user pressed	
+			var user = push.key;
+			userArray.push(" " + user);
+			// userGuess.textContent = push.key;
+
+
+			// turn the random math number into a letter from alphabet and set that letter to the variable comp
+			comp = alphabet[math];
+					
+					
+		
+		// if the value of user equals the value of comp, alert "winner" and advance wins by 1
+		// This works but it only advances once the next key is pressed.
+			if (user === comp) {
+				wins++;
+				alert("winner");
+				startOver();			
+			}
+		// if user does not equal comp, decrease the number of guesses left by 1
+			else if (user !== comp) {
+				guessesLeft--;
+			}
+		// this does not work, allows count to go below zero and does not alert or advance losses on zero	
+		// if the number of guesses equals zero, advance losses by 1 and alert "you lost"
+			if (guessesLeft === 0) {
+				losses++;
+				alert("You lost");
+				startOver();
+			}
+					
+				console.log("Wins:" + wins);
+				console.log("Losses:" +losses);
+				console.log("Comp Answer:" + comp);
+				console.log("Guesses Left:" + guessesLeft);
+				console.log("User Input:" + user);
+				console.log("--------")
+	
+	guessesText.textContent = guessesLeft;
+	lossesText.textContent = losses;
+	winsText.textContent = wins;
+	userGuess.textContent = userArray;
+};
+
+		
